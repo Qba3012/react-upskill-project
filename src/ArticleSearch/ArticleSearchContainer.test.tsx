bf1cde337160783e23a088620235a834f4304c73
jogorkiewicz <jogorkiewicz@objectivity.co.uk>
2021-07-26 13:42:51 +0200

    Add tests

---
+++ src/ArticleSearch/ArticleSearchContainer.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import ArticleSearchContainer from "./ArticleSearchContainer";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
+import TEST_DATA from "../wikipediaTestData.json";
+import { ApiContextProvider } from "../store/api-context";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("<ArticleSearchContainer> component", () => {
+  beforeEach(() => {
+      render(
+        <ApiContextProvider>
+          <ArticleSearchContainer />
+        </ApiContextProvider>
+      );
+  });
+
+  test("should render search bar", () => {
+    const element = screen.getByRole("textbox");
+    expect(element).toBeInTheDocument();
+  });
+
+  test("should render prompt text", () => {
+    const element = screen.getByText("Search in Wikipedia");
+    expect(element).toBeInTheDocument();
+  });
+
+  test("should not render article list if search box empty", () => {
+    const elements = screen.queryByRole("list");
+    expect(elements).not.toBeInTheDocument();
+  });
+
+  test("should article list if search box filled in", async () => {
+    mockedAxios.get.mockResolvedValueOnce(TEST_DATA);
+
+    const searchBox = screen.getByRole("textbox");
+    userEvent.type(searchBox, "test");
+
+    const elements = await screen.findAllByRole("listitem");
+    expect(elements).not.toHaveLength(0);
+  });
+
+  test("should not display prompt text if search box filled in", async () => {
+    mockedAxios.get.mockResolvedValueOnce(TEST_DATA);
+
+    const searchBox = screen.getByRole("textbox");
+    userEvent.type(searchBox, "test");
+
+    await screen.findAllByRole("listitem");
+    const element = screen.queryByText("Search in Wikipedia");
+    expect(element).toBeNull();
+  });
+});
--- src/ArticleSearch/ArticlesList.tsx
+++ src/ArticleSearch/ArticlesList.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@material-ui/core";
 import { FC, useContext } from "react";
 import ApiContext from "../store/api-context";
 import Article from "./Article";
@@ -6,7 +7,7 @@ const ArticlesList: FC = () => {
   const apiCtx = useContext(ApiContext);
 
   return (
-    <>
+    <Grid item container spacing={5} component="ul">
       {apiCtx.articlesList.map((article) => (
         <Article
           key={article.pageid}
@@ -15,7 +16,7 @@ const ArticlesList: FC = () => {
           url={article.url}
         />
       ))}
-    </>
+    </Grid>
   );
 };
 
